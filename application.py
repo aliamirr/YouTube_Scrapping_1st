9d2f7fef921857ebd0ce991bb0156d642961029e
Aamir <113689022+aliamirr@users.noreply.github.com>
2023-03-31 16:33:38 +0530

    Update application.py

--- application.py
+++ application.py
@@ -21,75 +21,75 @@ def index():
 def details():
 	if request.method == "POST":
 		try:
-			input_text = request.form['text']
-			url = f'https://www.youtube.com/@{input_text}/videos'
-			headers = {
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
-			'Accept-Language': 'en-US,en;q=0.9'
-			}
+		    input_text = request.form['text']
+		    url = f'https://www.youtube.com/@{input_text}/videos'
+		    headers = {
+		    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
+		    'Accept-Language': 'en-US,en;q=0.9'
+		    }
 
-			logging.info("Requesting HTML...")
-			response = requests.get(url, headers=headers)
-			response_text = response.text
+		    logging.info("Requesting HTML...")
+		    response = requests.get(url, headers=headers)
+		    response_text = response.text
 
-			#titles
-			logging.info("Finding titles...")
-			vid_titles = re.findall('"title":{"runs":\[{"text":".*?"', response_text)
+		    #titles
+		    logging.info("Finding titles...")
+		    vid_titles = re.findall('"title":{"runs":\[{"text":".*?"', response_text)
 
-			#thumbnails
-			logging.info("Finding thumbnails...")
-			vid_thumbnails = re.findall(r"https://i.ytimg.com/vi/[A-Za-z0-9_-]{11}/[A-Za-z0-9_]{9}.jpg", response_text)
+		    #thumbnails
+		    logging.info("Finding thumbnails...")
+		    vid_thumbnails = re.findall(r"https://i.ytimg.com/vi/[A-Za-z0-9_-]{11}/[A-Za-z0-9_]{9}.jpg", response_text)
 
-			#links
-			logging.info("Finding links...")
-			vid_links = re.findall(r"watch\?v=[A-Za-z0-9_-]{11}", response_text)
+		    #links
+		    logging.info("Finding links...")
+		    vid_links = re.findall(r"watch\?v=[A-Za-z0-9_-]{11}", response_text)
 
-			#view count
-			logging.info("Finding view counts...")
-			pattern3 = re.compile(r"[0-9]+(\.[0-9]+)?[a-zA-Z]*K views")
+		    #view count
+		    logging.info("Finding view counts...")
+		    pattern3 = re.compile(r"[0-9]+(\.[0-9]+)?[a-zA-Z]*K views")
 
-			#time posted
-			logging.info("Finding videos age...")
-			pattern4 = re.compile(r"\d+ (minutes|hours|hour|days|day|weeks|week|years|year) ago")
+		    #time posted
+		    logging.info("Finding videos age...")
+		    pattern4 = re.compile(r"\d+ (minutes|hours|hour|days|day|weeks|week|years|year) ago")
 
-			matches1 = pattern3.finditer(response_text)
-			matches2 = pattern4.finditer(response_text)
+		    matches1 = pattern3.finditer(response_text)
+		    matches2 = pattern4.finditer(response_text)
 
-			vid_viewcounts=[]
-			vid_ages=[]
-			count = 0
-			for match1,match2 in zip(matches1,matches2):
-				vid_ages.append(match2[0])
-				vid_viewcounts.append(match1[0])
+		    vid_viewcounts=[]
+		    vid_ages=[]
+		    count = 0
+		    for match1,match2 in zip(matches1,matches2):
+			    vid_ages.append(match2[0])
+			    vid_viewcounts.append(match1[0])
 
-			logging.info("Storing titles...")
-			titles = vid_titles[0:10]
-			logging.info("Storing thumbnails...")
-			thumbnails = list(dict.fromkeys(vid_thumbnails))
-			logging.info("Storing links...")
-			links = vid_links[0:10]
-			logging.info("Storing viewcounts...")
-			viewcounts=vid_viewcounts[0:20:2]
-			logging.info("Storing videos age...")
-			ages=vid_ages[0:20:2]
+		    logging.info("Storing titles...")
+		    titles = vid_titles[0:10]
+		    logging.info("Storing thumbnails...")
+		    thumbnails = list(dict.fromkeys(vid_thumbnails))
+		    logging.info("Storing links...")
+		    links = vid_links[0:10]
+		    logging.info("Storing viewcounts...")
+		    viewcounts=vid_viewcounts[0:20:2]
+		    logging.info("Storing videos age...")
+		    ages=vid_ages[0:20:2]
 
-			details_list=[]
+		    details_list=[]
 
-			for title,thumbnail,link,viewcount,age in zip(titles,thumbnails,links,viewcounts,ages):
-				details_dict={
-				"title":title.split('"')[-2], "thumbnail": thumbnail, "link": "https://www.youtube.com/"+link,
-				"viewcount": viewcount, "age": age
-				}
-				details_list.append(details_dict)
+		    for title,thumbnail,link,viewcount,age in zip(titles,thumbnails,links,viewcounts,ages):
+			    details_dict={
+			    "title":title.split('"')[-2], "thumbnail": thumbnail, "link": "https://www.youtube.com/"+link,
+			    "viewcount": viewcount, "age": age
+			    }
+			    details_list.append(details_dict)
 
-			#storing data into a csv file
-			df = pd.DataFrame(details_list)
-			df.to_csv('YTscrapData.csv', index=False)
+		    #storing data into a csv file
+		    df = pd.DataFrame(details_list)
+		    df.to_csv('YTscrapData.csv', index=False)
 
-			return render_template('details.html', details=details_list, channel=input_text)
-		
+		    return render_template('details.html', details=details_list, channel=input_text)
 
-		except Exception as e:
+
+	    except Exception as e:
 			print(e)
 
 if __name__ == "__main__":
